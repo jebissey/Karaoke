2d9712b9568bb9da2bfdbcf2203d2fc3ca35f602
jeb <je.bissey@gmail.com>
2023-05-26 10:29:26 +0200

    Code cleanup, moved files in Files directory, added SongFile in Settings.

--- Karaoke/MainWindow.xaml.cs
+++ Karaoke/MainWindow.xaml.cs
@@ -1,185 +1,157 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-using Microsoft.Win32;
-
+﻿using Karaoke.Properties;
 using NAudio.Wave;
-using NAudio.Wave.SampleProviders;
-
+using System;
+using System.Windows;
 using System.Windows.Threading;
 
-namespace reproductor
+namespace reproductor;
+
+public partial class MainWindow : Window
 {
-    
 
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    DispatcherTimer timer;
+
+    //Lector de archivos
+    AudioFileReader reader;
+    //Comunicacion con la tarjeta de audio
+    //Exclusivo para salida
+    WaveOut output;
+
+    bool dragging = false;
+
+    public MainWindow()
     {
+        InitializeComponent();
 
-        DispatcherTimer timer;
+        btnRerpoducir.IsEnabled = true;
+        btnDetener.IsEnabled = false;
+        btnPausa.IsEnabled = false;
 
-        //Lector de archivos
-        AudioFileReader reader;
-        //Comunicacion con la tarjeta de audio
-        //Exclusivo para salida
-        WaveOut output;
+        timer = new DispatcherTimer();
+        timer.Interval = TimeSpan.FromMilliseconds(500);
+        timer.Tick += Timer_Tick;
 
-        bool dragging = false;
+    }
 
-        public MainWindow()
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        lblTiempoActual.Text = reader.CurrentTime.ToString().Substring(0, 8);
+
+        if (!dragging)
         {
-            InitializeComponent();
-
-            btnRerpoducir.IsEnabled = true;
-            btnDetener.IsEnabled = false;
-            btnPausa.IsEnabled = false;
-
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(500);
-            timer.Tick += Timer_Tick;
-
+            sldTiempo.Value = reader.CurrentTime.TotalSeconds;
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
+        pbCancion.Value = reader.CurrentTime.TotalSeconds;
+
+
+        if (pbCancion.Value >= 1)
         {
-            lblTiempoActual.Text = reader.CurrentTime.ToString().Substring(0, 8);
-
-            if(!dragging)
-            {
-                sldTiempo.Value = reader.CurrentTime.TotalSeconds;
-            }
-
-            pbCancion.Value = reader.CurrentTime.TotalSeconds;
-
-
-            if (pbCancion.Value >= 1)
-            {
-                txtLyrics.Text = "In the town where I was born";
-            }
-
-            if (pbCancion.Value >= 6)
-            {
-                txtLyrics.Text = "Lived a man who sailed to sea";
-            }
-
-            if (pbCancion.Value >= 10.5)
-            {
-                txtLyrics.Text = "And he told us of his life";
-            }
-
-            if (pbCancion.Value >= 15)
-            {
-                txtLyrics.Text = "In the land of submarines";
-            }
+            txtLyrics.Text = "In the town where I was born";
         }
 
-  
-
-        
-
-        private void BtnRerpoducir_Click(object sender, RoutedEventArgs e)
+        if (pbCancion.Value >= 6)
         {
-
-            if (output != null && output.PlaybackState == PlaybackState.Paused)
-            {
-                //retomo reproduccion
-                output.Play();
-                btnRerpoducir.IsEnabled = true;
-                btnPausa.IsEnabled = true;
-                btnDetener.IsEnabled = true;
-            }
-            else
-            {
-                string testfile = @"yellow-submarine.mp3";
-                reader = new AudioFileReader(testfile);
-
-               
-
-                    output = new WaveOut();
-                    
-
-                    output.PlaybackStopped += Output_PlaybackStopped;
-
-                    output.Init(reader);
-                    output.Play();
-
-                    btnRerpoducir.IsEnabled = false;
-                    btnPausa.IsEnabled = true;
-                    btnDetener.IsEnabled = true;
-
-                    lblTiempoTotal.Text = reader.TotalTime.ToString().Substring(0, 8);
-                    lblTiempoActual.Text = reader.CurrentTime.ToString().Substring(0, 8);
-
-                    sldTiempo.Maximum = reader.TotalTime.TotalSeconds;
-                    sldTiempo.Value = reader.CurrentTime.TotalSeconds;
-
-                    pbCancion.Maximum = reader.TotalTime.TotalSeconds;
-
-                timer.Start();
-                
-            }
+            txtLyrics.Text = "Lived a man who sailed to sea";
         }
 
-        private void Output_PlaybackStopped(object sender, StoppedEventArgs e)
+        if (pbCancion.Value >= 10.5)
         {
-            timer.Stop();
-            reader.Dispose();
-            output.Dispose();
+            txtLyrics.Text = "And he told us of his life";
         }
 
-     
-
-        private void BtnDetener_Click(object sender, RoutedEventArgs e)
+        if (pbCancion.Value >= 15)
         {
-            if (output != null)
-            {
-                output.Stop();
-                btnRerpoducir.IsEnabled = true;
-                btnPausa.IsEnabled = false;
-                btnDetener.IsEnabled = false;
-            }
-        }
-
-        private void BtnPausa_Click(object sender, RoutedEventArgs e)
-        {
-            if(output != null)
-            {
-                output.Pause();
-                btnRerpoducir.IsEnabled = true;
-                btnPausa.IsEnabled = false;
-                btnDetener.IsEnabled = true;
-            }
-        }
-
-        private void SldTiempo_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
-        {
-            dragging = true;
-
-        }
-
-        private void SldTiempo_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
-        {
-            dragging = false;
-
-            if(reader != null && output != null && output.PlaybackState != PlaybackState.Stopped)
-            {
-                reader.CurrentTime = TimeSpan.FromSeconds(sldTiempo.Value);
-            }
+            txtLyrics.Text = "In the land of submarines";
         }
     }
-        
+
+
+
+
+
+    private void BtnRerpoducir_Click(object sender, RoutedEventArgs e)
+    {
+
+        if (output != null && output.PlaybackState == PlaybackState.Paused)
+        {
+            //retomo reproduccion
+            output.Play();
+            btnRerpoducir.IsEnabled = true;
+            btnPausa.IsEnabled = true;
+            btnDetener.IsEnabled = true;
+        }
+        else
+        {
+            reader = new AudioFileReader(Settings.Default.SongFile);
+            output = new WaveOut();
+
+
+            output.PlaybackStopped += Output_PlaybackStopped;
+
+            output.Init(reader);
+            output.Play();
+
+            btnRerpoducir.IsEnabled = false;
+            btnPausa.IsEnabled = true;
+            btnDetener.IsEnabled = true;
+
+            lblTiempoTotal.Text = reader.TotalTime.ToString().Substring(0, 8);
+            lblTiempoActual.Text = reader.CurrentTime.ToString().Substring(0, 8);
+
+            sldTiempo.Maximum = reader.TotalTime.TotalSeconds;
+            sldTiempo.Value = reader.CurrentTime.TotalSeconds;
+
+            pbCancion.Maximum = reader.TotalTime.TotalSeconds;
+
+            timer.Start();
+
+        }
+    }
+
+    private void Output_PlaybackStopped(object sender, StoppedEventArgs e)
+    {
+        timer.Stop();
+        reader.Dispose();
+        output.Dispose();
+    }
+
+
+
+    private void BtnDetener_Click(object sender, RoutedEventArgs e)
+    {
+        if (output != null)
+        {
+            output.Stop();
+            btnRerpoducir.IsEnabled = true;
+            btnPausa.IsEnabled = false;
+            btnDetener.IsEnabled = false;
+        }
+    }
+
+    private void BtnPausa_Click(object sender, RoutedEventArgs e)
+    {
+        if (output != null)
+        {
+            output.Pause();
+            btnRerpoducir.IsEnabled = true;
+            btnPausa.IsEnabled = false;
+            btnDetener.IsEnabled = true;
+        }
+    }
+
+    private void SldTiempo_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+    {
+        dragging = true;
+    }
+
+    private void SldTiempo_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+    {
+        dragging = false;
+
+        if (reader != null && output != null && output.PlaybackState != PlaybackState.Stopped)
+        {
+            reader.CurrentTime = TimeSpan.FromSeconds(sldTiempo.Value);
+        }
+    }
 }
--- Karaoke/Properties/Settings.Designer.cs
+++ Karaoke/Properties/Settings.Designer.cs
@@ -8,22 +8,27 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace reproductor.Properties
-{
-
-
+namespace Karaoke.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("./Files/yellow-submarine.mp3")]
+        public string SongFile {
+            get {
+                return ((string)(this["SongFile"]));
             }
         }
     }
